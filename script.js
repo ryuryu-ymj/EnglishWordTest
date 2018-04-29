3facee4ee6739ce1548af9975a1c42dc90febff8
ryuryu <ryuryu.ymj@gmail.com>
2018-04-29 21:16:24 +0900

    create checkAnswer function

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 console.log("ryuryu");
 
 var english = ["apple", "orange", "pen"];
-var englishNum = 0;
+var japanese = ["りんご", "オレンジ", "ペン"];
+var problemNum = 0;
+
+document.getElementById("question").textContent = japanese[problemNum];
 
 document.getElementById("form").onsubmit = function()
 {
-	document.getElementById("english").textContent
-		= english[++englishNum];
-	var answer = getElementById("form").answer_input.value;
+    var answer = document.getElementById("form").answer_input.value;
 	console.log(answer);
+	if (answer === english[problemNum])
+	{
+	    console.log("正解");
+	    document.getElementById("question").textContent = japanese[++problemNum];
+	    document.getElementById("form").answer_input.value = "";
+	}
 	return false;
-}
+};
+
+var checkAnswer() = function()
+{
+    var answer = document.getElementById("form").answer_input.value;
+	console.log(answer);
+	if (answer === english[problemNum])
+	{
+	    console.log("正解");
+	    document.getElementById("question").textContent = japanese[++problemNum];
+	    document.getElementById("form").answer_input.value = "";
+	}
+};
+
+var checkAnswer(var keyCode) = function()
+{
+    if (keyCode === 13)
+    {
+        checkAnswer();
+    }
+};
 
