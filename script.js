f4483d97c5e42d642d44ed15a26ffed0b1eee091
ryuryu <ryuryu.ymj@gmail.com>
2018-05-25 18:20:03 +0900

    add displayScore

--- script.js
+++ script.js
@@ -2,24 +2,73 @@ console.log("ryuryu");
 
 var english = ["apple", "orange", "pen"];
 var japanese = ["りんご", "オレンジ", "ペン"];
+var isCorrect = new Array(english.length);
 var problemNum = 0;
+var isDisplaySolution = false;
 
 document.getElementById("question").textContent = japanese[problemNum];
 
 document.getElementById("form").onsubmit = function()
 {
-    var answer = document.getElementById("form").answer_input.value;
-	console.log(answer);
-	if (answer === english[problemNum])
+	if (isDisplaySolution === true)
 	{
-	    console.log("正解");
-	    document.getElementById("question").textContent = japanese[++problemNum];
-	    document.getElementById("form").answer_input.value = "";
+		isDisplaySolution = false;
+		document.getElementById("question").style.color = "black";
+		document.getElementById("question").textContent = japanese[++problemNum];
+		document.getElementById("form").answer_input.value = "";
+	}
+	else
+	{
+		var answer = document.getElementById("form").answer_input.value;
+		console.log(answer);
+		if (answer === english[problemNum])
+		{
+			console.log("正解");
+			isCorrect[problemNum] = true;
+			document.getElementById("question").textContent = "〇 " + english[problemNum];
+			document.getElementById("question").style.color = "limegreen";
+			isDisplaySolution = true;
+		}
+		else
+		{
+			console.log("不正解");
+			isCorrect[problemNum] = false;
+			document.getElementById("question").textContent = "✖ " + english[problemNum];
+			document.getElementById("question").style.color = "red";
+			isDisplaySolution = true;
+		}
+	}
+	if (problemNum === english.length)
+	{
+		displayScore();
 	}
 	return false;
-};
+}
 
-var checkAnswer() = function()
+function displayScore()
+{
+	console.log("finish");
+	var scoreBoard = "<h3>テスト結果</h3><ol>";
+	var score = 0;
+	for (var i = 0; i < english.length; i++)
+	{
+		scoreBoard += "<li>" + japanese[i] + "  " + english[i];
+		if (isCorrect[i] === true)
+		{
+			scoreBoard += '<span style="color:limegreen"> 〇</span>';
+			score++;
+		}
+		else
+		{
+			scoreBoard += '<span style="color:red"> ✖</span>';
+		}
+		scoreBoard += "</li>";
+	}
+	scoreBoard += "</ol><p>" + score + "/" + english.length + "　点</p>";
+	document.getElementById("main").innerHTML = scoreBoard;
+}
+
+/*var checkAnswer() = function()
 {
     var answer = document.getElementById("form").answer_input.value;
 	console.log(answer);
@@ -29,7 +78,7 @@ var checkAnswer() = function()
 	    document.getElementById("question").textContent = japanese[++problemNum];
 	    document.getElementById("form").answer_input.value = "";
 	}
-};
+}
 
 var checkAnswer(var keyCode) = function()
 {
@@ -37,5 +86,5 @@ var checkAnswer(var keyCode) = function()
     {
         checkAnswer();
     }
-};
+}*/
 
